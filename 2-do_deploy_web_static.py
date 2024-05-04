98c02499079387eab097c8875f4490aa1278d8e6
Mohammed <mrcloud.med@gmail.com>
2024-05-04 01:57:17 +0100

    pycodestyle

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -20,13 +20,13 @@ def do_deploy(archive_path):
         put(archive_path, "/tmp/")
         run("mkdir -p /data/web_static/releases/")
         file_name = archive_path.split("/")[-1]
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/"\
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/"
             .format(file_name))
         run("rm /tmp/{}".format(file_name))
-        run("mv /data/web_static/releases/web_static /data/web_static/\
-            releases/{}".format(file_name))
+        run("mv /data/web_static/releases/web_static /data/web_static/releases/{}"
+            .format(file_name))
         run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{} /data/web_static/current"\
+        run("ln -s /data/web_static/releases/{} /data/web_static/current"
             .format(file_name))
         return True
     except:
