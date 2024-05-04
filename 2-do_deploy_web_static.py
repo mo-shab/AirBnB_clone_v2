4a3a490465f0c92bb6f8372697cdcd1e3536d85e
Mohammed <mrcloud.med@gmail.com>
2024-05-04 02:00:25 +0100

    Deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -17,17 +17,17 @@ def do_deploy(archive_path):
     if exists(archive_path) is False:
         return False
     try:
-        put(archive_path, "/tmp/")
-        run("mkdir -p /data/web_static/releases/")
         file_name = archive_path.split("/")[-1]
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/"
-            .format(file_name))
-        run("rm /tmp/{}".format(file_name))
-        run("mv /data/web_static/releases/web_static /data/web_static/releases/{}"
-            .format(file_name))
-        run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{} /data/web_static/current"
-            .format(file_name))
+        no_ext = file_name.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_name, path, no_ext))
+        run('rm /tmp/{}'.format(file_name))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
     except:
         return False
