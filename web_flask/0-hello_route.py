5ae5eac52a3175aa9f8ae732fed7e99be0e880d6
Mohammed <mrcloud.med@gmail.com>
2024-05-17 15:36:39 +0200

    Documentation

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,7 +7,9 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def Hello_HBNB():
+    """Function Hello HBNB"""
     return 'Hello HBNB!'
 
 if __name__ == "__main__":
+    """Main Function to run the app"""
     app.run(host="0.0.0.0", port=5000)
