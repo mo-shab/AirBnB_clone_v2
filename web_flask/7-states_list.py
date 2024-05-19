a80e45de52b1157ad85d9c406df32bb587e952f8
Mohammed <mrcloud.med@gmail.com>
2024-05-19 22:33:38 +0200

    State list

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Module that List states"""
+
+
+from flask import Flask, render_template
+from models import *
+from models import storage
+
+
+app = Flask(__name__)
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """Function that display a list of states"""
+    states = sorted(list(storage.all("State").value()), key=lambda x: x.name)
+    return render_template('7-states_list.html', states=states)
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Close the storage on reardown after each request"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    """Main function"""
+    app.run(host="0.0.0.0", port=5000)
