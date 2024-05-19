9da9c6af748d36d0a37945cd7ca93c61c3a5bb6a
Mohammed <mrcloud.med@gmail.com>
2024-05-19 22:45:11 +0200

    Fix states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """Function that display a list of states"""
-    states = sorted(list(storage.all(State).value()), key=lambda x: x.name)
+    states = storage.all(State).values()
     return render_template('7-states_list.html', states=states)
 
 
