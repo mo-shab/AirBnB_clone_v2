0c93f536e03ef6b973112ee34846ea9927f6c55e
Mohammed <mrcloud.med@gmail.com>
2024-05-20 01:01:20 +0200

    Fix html

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -21,5 +21,13 @@ def hbnb_filters():
                            amenities=amenities)
 
 
+def handle_teardown(exception):
+    """
+        method to handle teardown
+    """
+    storage.close()
+
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
